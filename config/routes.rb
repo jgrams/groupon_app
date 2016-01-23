7b439967152debe497ff738518302cbca44b333e
John Gramila <tynanjk@gmail.com>
2016-01-23 14:58:28 -0600

    Updated Root Route again to a different site

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'first_inputs#index'
+  root 'first_inputs#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
