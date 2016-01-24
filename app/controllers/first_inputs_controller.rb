96bc63ab5223f72bd0e0bee3ce76b697a5088a91
John Gramila <tynanjk@gmail.com>
2016-01-24 10:33:24 -0600

    Got it working, not RESTFUL

--- app/controllers/first_inputs_controller.rb
+++ app/controllers/first_inputs_controller.rb
@@ -22,6 +22,43 @@ class FirstInputsController < ApplicationController
   # GET /first_inputs/1
   # GET /first_inputs/1.json
   def show
+  	
+  	if @first_input.num_options == 1 && @first_input.deal_type == "Gen Spend" && @first_input.multi_voucher == false
+  		@gallery_title = "$pr for $va for " << @first_input.longer_descriptor
+			@title = "$pr for $va Worth of " << @first_input.longer_descriptor << " from " << @first_input.biz_name
+			@descriptor = @first_input.longer_descriptor
+			@short_descriptor = @first_input.longer_descriptor
+			@writeup = "####The Deal * $pr for a " << @first_input.longer_descriptor << " (a $val value)".downcase
+  	
+  	elsif @first_input.num_options >= 2 && @first_input.deal_type == "Gen Spend" && @first_input.multi_voucher == false
+  		@gallery_title = "Up to $max Off " << @first_input.longer_descriptor
+			@title = @first_input.longer_descriptor << " from " << @first_input.biz_name << " ($maximum_of_discount_percentage Off). " << Article::NUMERALS[@first_input.num_options] << " Options Available."
+			@descriptor = @first_input.longer_descriptor
+			@short_descriptor = @first_input.longer_descriptor
+			@writeup = "####The Deal * $pr for a " << @first_input.longer_descriptor << " (a $val value)".downcase
+  	
+  	elsif @first_input.num_options == 1 && @first_input.deal_type == "Product Deal" && @first_input.multi_voucher == false
+  		@gallery_title = "$pr for a " << @first_input.longer_descriptor
+			@title = "$pr for a " << @first_input.longer_descriptor << " from " << @first_input.biz_name << " ($val Value)"
+			@descriptor = @first_input.longer_descriptor
+			@short_descriptor = @first_input.longer_descriptor
+			@writeup = "####The Deal * $pr for a " << @first_input.longer_descriptor << " (a $val value)".downcase
+  	
+  	elsif @first_input.num_options >= 2 && @first_input.deal_type == "Product Deal" && @first_input.multi_voucher == false	
+  		@longer_descriptor = @first_input.longer_descriptor
+  	
+  	elsif @first_input.num_options == 1 && @first_input.deal_type == "Gen Spend" && @first_input.multi_voucher == true
+  		@longer_descriptor = @first_input.longer_descriptor
+  	
+  	elsif @first_input.num_options >= 2 && @first_input.deal_type == "Gen Spend" && @first_input.multi_voucher == true
+  		@longer_descriptor = @first_input.longer_descriptor
+  	
+  	elsif @first_input.num_options == 1 && @first_input.deal_type == "Product Deal" && @first_input.multi_voucher == true
+  		@longer_descriptor = @first_input.longer_descriptor
+  	
+  	elsif @first_input.num_options >= 2 && @first_input.deal_type == "Product Deal" && @first_input.multi_voucher == true
+  		@longer_descriptor = @first_input.longer_descriptor
+  	end
   end
 
   # GET /first_inputs/new
@@ -32,6 +69,27 @@ class FirstInputsController < ApplicationController
   # GET /first_inputs/1/edit
   def edit
   end
+
+  def route
+  	if @first_input.num_options == 1 && @first_input.deal_type == "Gen Spend" && @first_input.multi_voucher == false
+  		@longer_descriptor = @first_input.longer_descriptor
+  	elsif @first_input.num_options >= 2 && @first_input.deal_type == "Gen Spend" && @first_input.multi_voucher == false
+  		@longer_descriptor = @first_input.longer_descriptor
+  	elsif @first_input.num_options == 1 && @first_input.deal_type == "Product Deal" && @first_input.multi_voucher == false
+  		@longer_descriptor = @first_input.longer_descriptor
+  	elsif @first_input.num_options >= 2 && @first_input.deal_type == "Product Deal" && @first_input.multi_voucher == false	
+  		@longer_descriptor = @first_input.longer_descriptor
+  	elsif @first_input.num_options == 1 && @first_input.deal_type == "Gen Spend" && @first_input.multi_voucher == true
+  		@longer_descriptor = @first_input.longer_descriptor
+  	elsif @first_input.num_options >= 2 && @first_input.deal_type == "Gen Spend" && @first_input.multi_voucher == true
+  		@longer_descriptor = @first_input.longer_descriptor
+  	elsif @first_input.num_options == 1 && @first_input.deal_type == "Product Deal" && @first_input.multi_voucher == true
+  		@longer_descriptor = @first_input.longer_descriptor
+  	elsif @first_input.num_options >= 2 && @first_input.deal_type == "Product Deal" && @first_input.multi_voucher == true
+  		@longer_descriptor = @first_input.longer_descriptor
+  	end
+  end
+
 
   # POST /first_inputs
   # POST /first_inputs.json
@@ -83,6 +141,8 @@ class FirstInputsController < ApplicationController
     def first_input_params
       params.require(:first_input).permit(:num_options, :deal_type, :biz_name, :longer_descriptor, :multi_voucher)
     end
+
+
 
 		def output_chooser
       if :deal_type == "Gen Spend"  && :num_options == 1  && :multi_voucher == false
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'title_fields/output'
 
+
+
   resources :first_inputs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
