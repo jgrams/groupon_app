eace259cbf27e8aab09cdfa36d73be36ed9d34bb
John Gramila <tynanjk@gmail.com>
2016-01-25 12:19:57 -0600

    site ready build for upload

--- app/controllers/first_inputs_controller.rb
+++ app/controllers/first_inputs_controller.rb
@@ -19,32 +19,13 @@ class FirstInputsController < ApplicationController
   # GET /first_inputs/1.json
   def show
   	if @first_input.num_options == 1 && @first_input.deal_type == "Gen Spend" && @first_input.multi_voucher == false
-  		
-  		@gallery_title = "$pr for $va for " << @first_input.longer_descriptor
-			@title = "$pr for $va Worth of " << @first_input.longer_descriptor << " from " << @first_input.biz_name
-			@descriptor = @first_input.longer_descriptor
-			@short_descriptor = @first_input.longer_descriptor
-			@writeupheader = "####The Deal"
-			@writeup = " * $pr for a " << @first_input.longer_descriptor.downcase << " (a $val value)"
-			  	
-  	elsif @first_input.num_options >= 2 && @first_input.deal_type == "Gen Spend" && @first_input.multi_voucher == false
-  		@gallery_title = "Up to $max Off " << @first_input.longer_descriptor
-			@title = @first_input.longer_descriptor << " from " << @first_input.biz_name << " ($maximum_of_discount_percentage Off). " << Article::NUMERALS[@first_input.num_options] << " Options Available."
-			@descriptor = @first_input.longer_descriptor
-			@short_descriptor = @first_input.longer_descriptor
-			@writeupheader = "####The Deal"
-			@writeup = " * $pr for a " << @first_input.longer_descriptor.downcase << " (a $val value)"
-  	
+  		gensped_oneoption
+		elsif @first_input.num_options >= 2 && @first_input.deal_type == "Gen Spend" && @first_input.multi_voucher == false
+		  genspend_multoption
   	elsif @first_input.num_options == 1 && @first_input.deal_type == "Product Deal" && @first_input.multi_voucher == false
-  		@gallery_title = "$pr for a " << @first_input.longer_descriptor
-			@title = "$pr for a " << @first_input.longer_descriptor << " from " << @first_input.biz_name << " ($val Value)"
-			@descriptor = @first_input.longer_descriptor
-			@short_descriptor = @first_input.longer_descriptor
-			@writeupheader = "####The Deal"
-			@writeup = " * $pr for a " << @first_input.longer_descriptor.downcase << " (a $val value)"
-  	
+			proddeal_oneoption
   	elsif @first_input.num_options >= 2 && @first_input.deal_type == "Product Deal" && @first_input.multi_voucher == false	
-  		@longer_descriptor = @first_input.longer_descriptor
+  		proddeal_multoption
   	
   	elsif @first_input.num_options == 1 && @first_input.deal_type == "Gen Spend" && @first_input.multi_voucher == true
   		@longer_descriptor = @first_input.longer_descriptor
@@ -118,7 +99,7 @@ class FirstInputsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def first_input_params
-      params.require(:first_input).permit(:num_options, :deal_type, :biz_name, :longer_descriptor, :multi_voucher)
+      params.require(:first_input).permit(:num_options, :deal_type, :biz_name, :longer_descriptor, :multi_voucher, :multoption_types, :optionals, :option_descriptor, :option_multiplier)
     end
 
 end
--- app/models/first_input.rb
+++ app/models/first_input.rb
@@ -14,7 +14,7 @@ class Article < ActiveRecord::Base
 end
 
 class Article < ActiveRecord::Base
-	MULTIPLIERTYPE = ['Short Descriptor', 'People', 'Minutes', 'Hours', 'Days', 'Weeks', 'Months', 'Years']
+	MULTIPLIERTYPE = ['Short Descriptor', 'People', 'Minute', 'Hour', 'Day', 'Week', 'Month', 'Year']
 end
 
 #array output functions use to convert integer inputs into string outputs
@@ -23,11 +23,61 @@ class Article < ActiveRecord::Base
 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen']
 end
 
+class Article < ActiveRecord::Base
+	NUMERALS = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 
+'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen']
+end
 
+
+#Output functions called by show in controller
 def gensped_oneoption
   	@gallery_title = "$pr for $va for " << @first_input.longer_descriptor
 		@title = "$pr for $va Worth of " << @first_input.longer_descriptor << " from " << @first_input.biz_name
 		@descriptor = @first_input.longer_descriptor
 		@short_descriptor = @first_input.longer_descriptor
-		@writeup = "####The Deal * $pr for a " << @first_input.longer_descriptor << " (a $val value)".downcase
+		@writeupheader = "####The Deal"
+		@writeup = " * $pr for $val worth of " << @first_input.longer_descriptor.downcase
+end
+
+def genspend_multoption
+  	@gallery_title = "Up to $max Off " << @first_input.longer_descriptor
+		@title = "" << @first_input.longer_descriptor << " from " << @first_input.biz_name << " ($max Off). " << Article::NUMERALS[@first_input.num_options] << " Options Available."
+		@descriptor = @first_input.longer_descriptor			
+		@short_descriptor = @first_input.longer_descriptor
+		@writeupheader = "####Choose Between " << Article::NUMERALS[@first_input.num_options] << " Options"
+		@writeup = (" * $pr for " << @first_input.longer_descriptor.downcase << " (a $val value)")
+end
+
+def proddeal_oneoption
+  		@gallery_title = "$pr for a " << @first_input.longer_descriptor
+			@title = "$pr for a " << @first_input.longer_descriptor << " from " << @first_input.biz_name << " ($val Value)"
+			@descriptor = @first_input.longer_descriptor
+			@short_descriptor = @first_input.longer_descriptor
+			@writeupheader = "####The Deal"
+			@writeup = " * $pr for a " << @first_input.longer_descriptor.downcase << " (a $val value)"
+end
+
+
+#THIS FUNCTION NEEDS SOME TLC
+def proddeal_multoption
+	@gallery_title = "Up to $max Off " << @first_input.longer_descriptor
+	@descriptor = @first_input.longer_descriptor
+	@short_descriptor = @first_input.longer_descriptor
+	@writeupheader = "####Choose Between " << Article::NUMERALS[@first_input.num_options] << " Options"
+	@writeup = " * $pr for " << @first_input.longer_descriptor.downcase << " (a $val value)"
+	if @first_input.multoption_types == "Multiplied" 
+		
+			if @first_input.optionals == "Short Descriptor"
+				@title = "Up to $max Off " << @first_input.option_multiplier.split(' ').map(&:to_i).collect{|x| Article::NUMERALS[x]}.join(" or ") << " " << @first_input.longer_descriptor << " from " << @first_input.biz_name
+			elsif @first_input.optionals == "People"
+				@title = "Up to $max Off " << @first_input.longer_descriptor << " for " << @first_input.option_multiplier.split(' ').map(&:to_i).collect{|x| Article::NUMERALS[x]}.join(" or ") << " " << @first_input.optionals << " from " << @first_input.biz_name
+#PARTICULARLY THIS			
+			else
+				@title = "Up to $max Off " << @first_input.option_multiplier.split(' ').map(&:to_i).join(" or ") << "  " << @first_input.optionals << " " << @first_input.longer_descriptor << " from " << @first_input.biz_name
+			end
+	elsif @first_input.multoption_types == "Optional"
+		@title = "Up to $max Off " << @first_input.longer_descriptor << " With Optional " << @first_input.option_descriptor.split(", ")[0..-1].join(" or ").to_s << " from " << @first_input.biz_name
+	elsif @first_input.multoption_types == "Complicated"
+		@title = "Up to $max Off " << @first_input.longer_descriptor << " from " << @first_input.biz_name << ". " << Article::NUMERALS[@first_input.num_options] << " Options Available."
+	end
 end
